74244c6dc0798be09e6395941ebf6f659395b420
Eric J Fisher <EricJFIsher@Live.com>
2018-03-09 14:32:33 -0500

    Completed views test with Regex
    Experiments using Selenium showed absolutely horrendous performance.
    WaTin had better performance, but still not great and also only works
    on Windows which makes it a solid no go for projects. Finally the
    view’s content appears to require spinning up and consuming from the
    web host to accurately test which isn’t cross platform friendly, for
    the time being this means we’ve had to fall back to regex for testing
    views, for ASP.NET Core Project 1 that should be "good enough" for now.
    I’m still researching other possibilities because Project 3 will be UI
    heavy and testing it all via regex is likely to go poorly with all the
    potential for valid fringe cases.

--- SampleXunitTestsProject/SampleViewTest.cs
+++ SampleXunitTestsProject/SampleViewTest.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using Xunit;
+
 namespace SampleXunitTestsProject
 {
     public class SampleViewTest
     {
-        /// <summary>
-        ///     Sample test
-        /// </summary>
-        public SampleViewTest()
+        [Fact(DisplayName = "Create Index View @create-index-view")]
+        public void CreateIndexViewTest()
         {
+            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "SampleASPNetCoreProject" + Path.DirectorySeparatorChar + "Views" + Path.DirectorySeparatorChar + "Sample" + Path.DirectorySeparatorChar + "Index.cshtml";
+            string file;
+            using(StreamReader streamReader = new StreamReader(filePath))
+            {
+                file = streamReader.ReadToEnd();
+            }
+
+            var pattern = @"<\s?h1\s?>.*</\s?h1\s?>";
+            var rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            Assert.True(rgx.IsMatch(file), "`Index.cshtml` did not contain a valid opening and closing `h1` tags.");
+        }
+
+        [Fact(DisplayName = "Create Details View @create-details-view")]
+        public void CreateDetailsViewTest()
+        {
+            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "SampleASPNetCoreProject" + Path.DirectorySeparatorChar + "Views" + Path.DirectorySeparatorChar + "Sample" + Path.DirectorySeparatorChar + "Details.cshtml";
+            string file;
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                file = streamReader.ReadToEnd();
+            }
+
+            var pattern = @"@model\sstring((\n|.)*)<\s?[hH]1\s?>.*</\s?[hH]1\s?>\s?<\s?[pP]\s?>\s?@Model\s?</\s?[pP]\s?>";
+            var rgx = new Regex(pattern);
+
+            Assert.True(rgx.IsMatch(file), "`Details.cshtml` did not contain the expected model declaration, `h1` tag containing the value 'Details', and `p` tag with a value of `@Model`.");
         }
     }
 }
